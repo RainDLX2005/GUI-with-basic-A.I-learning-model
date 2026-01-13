14f3634c685d2dd10d40c996f95203309350fa4b
RainDLX2005 <tranhaokhiem9a5@gmail.com>
2026-01-13 22:55:04 +0700

    Update regression.py

--- GUI/regression.py
+++ GUI/regression.py
@@ -10,7 +10,6 @@ def load_training_data_csv(path="df_weather.csv"):
     with open(path, "r", encoding="utf-8") as f:
         reader = csv.DictReader(f)
 
-        # 👉 CỘT NHIỆT ĐỘ CHUẨN
         TEMP_COL = "day.avgtemp_c"
 
         for row in reader:
@@ -23,13 +22,12 @@ def load_training_data_csv(path="df_weather.csv"):
     return X, y
 
 
-def predict_next_24h_from_csv(csv_path="df_weather.csv"):
+def predict_from_csv(csv_path="df_weather.csv"):
     X, y = load_training_data_csv(csv_path)
 
     model = LinearRegression()
     model.fit(X, y)
 
-    # ⚠️ Vì data là THEO NGÀY → dự đoán 8 NGÀY TIẾP
     next_X = np.arange(len(y), len(y) + 8).reshape(-1, 1)
     predicted = model.predict(next_X)
 
